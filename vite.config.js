69f6b878d3efafd53c542454ddb906d45443b971
Jan Michael Caasi <janmichaelcaasi@gmail.com>
2024-06-15 06:10:36 +0800

    added nginx

--- vite.config.js
+++ vite.config.js
@@ -6,11 +6,6 @@ export default defineConfig({
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
-            server: {
-                host: true,
-                strictPort: true,
-                port: 8000,
-                },
         }),
     ],
 });
