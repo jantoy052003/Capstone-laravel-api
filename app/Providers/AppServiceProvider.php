9d36e46a91f7208a6c7867f278d6d91cc13a9a66
Jan Michael Caasi <janmichaelcaasi@gmail.com>
2024-06-15 15:40:45 +0800

    added autoload

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -18,8 +18,10 @@ class AppServiceProvider extends ServiceProvider
     /**
      * Bootstrap any application services.
      */
-    public function boot()
+    public function boot(UrlGenerator $url)
     {
-        
+        if (env('APP_ENV') == 'production') {
+            $url->forceScheme('https');
+        }
     }
 }
